0465a5ea5184aa646756a5c151aabfaefcb3cce6
lucialence <51814619+lucialence@users.noreply.github.com>
2022-12-21 13:57:36 +0100

    Css classes added

--- adiante-front-office/src/views/LoginView.vue
+++ adiante-front-office/src/views/LoginView.vue
@@ -6,12 +6,12 @@
       <form @submit="handleLogin">
         <div class="form-login">
           <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Usuario</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <label for="InputEmail1" class="form-label">Usuario</label>
+            <input type="text" class="form-control" id="InputEmail1" aria-describedby="emailHelp">
            </div>
           <div class="mb-4">
-            <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <label for="InputPassword1" class="form-label">Contraseña</label>
+            <input type="password" class="form-control" id="InputPassword1">
           </div>
           <button type="submit" class="btn btn-primary w-100">Acceder <span class="material-symbols-outlined">
             login
