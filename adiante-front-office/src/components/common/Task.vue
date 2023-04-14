767e097494268893cdd01744785028adc3efd95f
lucialence <51814619+lucialence@users.noreply.github.com>
2023-04-15 01:01:46 +0200

    components ui

---
+++ adiante-front-office/src/components/common/Task.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="task" data-bs-toggle="modal" data-bs-target="#type-2">
+        <div class="header">
+            <h4>Título de la tarea</h4>
+          <div class="state delayed"><span> No completada</span></div>
+        </div>
+        <div class="content">
+          <div class="task-info date"><span class="icon material-symbols-outlined"></span> 00/00/0000 </div>
+          <div class="task-info time"><span class="icon material-symbols-outlined"></span> 11:00 </div>
+        </div>
+        <div class="details">
+          <p>Esta es la descripción larga de la tarea en donde se explica al usuario de manera detallada en qué
+            consiste la tarea que debe realizar.</p>
+        </div>
+        <div class="alert error" role="alert">
+          <p>Esta tarea tiene un retraso de 2h.</p>
+        </div>
+      </div>
+  
+   </template> 
+   
+         <script>
+   export default {
+     name: 'Task',
+     props: {
+       msg: String
+     }
+   }
+   </script>
+    
---
+++ adiante-front-office/src/views/dDashboardView.vue
@@ -0,0 +1,48 @@
+<script>
+import HeaderMenu from '../components/common/HeaderMenu.vue'
+import FooterMenu from '../components/common/FooterMenu.vue'
+import WidgetHello from '../components/WidgetHello.vue'
+import WidgetState from '../components/WidgetState.vue'
+import WidgetTask from '../components/WidgetTask.vue'
+import WidgetWeek from '../components/WidgetWeek.vue'
+import Card from '../components/common/Card.vue'
+import Task from '../components/common/Task.vue'
+
+export default {
+  components: {
+    HeaderMenu: HeaderMenu,
+    FooterMenu: FooterMenu,
+    WidgetHello: WidgetHello,
+    WidgetState: WidgetState,
+    WidgetTask: WidgetTask,
+    WidgetWeek: WidgetWeek,
+    Card: Card,
+    Task: Task
+  }
+}
+</script>
+
+<template>
+  <HeaderMenu />
+  <div class="app">
+
+    <WidgetHello />
+    <WidgetState />
+    <WidgetTask />
+    
+    <Card />
+
+    <WidgetWeek />
+
+    <Task />
+
+    <a href="" data-bs-toggle="modal" data-bs-target="#modalid"> abre modal</a>
+
+    <Modal />
+
+  </div>
+ 
+  <FooterMenu />
+
+</template>
+   
