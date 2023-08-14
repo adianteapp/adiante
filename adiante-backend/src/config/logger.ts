654fc14ffad92cb16c702523b5f0cd45eabfe8a6
MIGUEL ANDRADE VAZQUEZ <miguel.andrade@luckia.com>
2023-08-14 18:41:02 +0200

    Added configuration to log errors through Grafana

--- adiante-backend/src/config/logger.ts
+++ adiante-backend/src/config/logger.ts
@@ -4,6 +4,7 @@ import TransportStream from 'winston-transport'
 import LokiTransport from 'winston-loki'
 import * as dotenv from 'dotenv';
 import DailyRotateFile from 'winston-daily-rotate-file';
+import e from 'express';
 
 dotenv.config();
 
@@ -14,6 +15,32 @@ const levels = {
   http: 3,
   debug: 4,
 }
+
+
+const logTransports = {
+  lokiTransport: new LokiTransport({
+    host: `${process.env.API_LOG_GRAFANA_LOKI_URL}`,
+    labels: { app: `${process.env.APP_NAME}` },
+    json: true,
+    level: `${process.env.API_LOG_FILE_LOKI_TRANSPORT_LEVEL}`,
+    format: winston.format.json(),
+    replaceTimestamp: true,
+    onConnectionError: (err) => console.error(err)
+  }),
+  consoleTransport: new winston.transports.Console(),
+  fileTransport: new DailyRotateFile({
+    filename: `${process.env.API_LOG_FILE_AUDIT_NAME}`,
+    dirname: `${process.env.API_LOG_PATH}`,
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    level: `${process.env.API_LOG_FILE_TRANSPORT_LEVEL}`,
+    maxSize: `${process.env.API_LOG_MAX_SIZE}`,
+    maxFiles: `${process.env.API_LOG_MAX_DAYS}`
+  })
+}
+
+
+
 
 const level = () => {
   const env = process.env.NODE_ENV || 'development'
@@ -41,41 +68,38 @@ const errorTransport = new DailyRotateFile({
 
 let transports: TransportStream[] = [];
 
-if( `${process.env.API_LOG_FILE_LOKI_TRANSPORT_ENABLED}` == '1'){
-  transports.push(new LokiTransport({
-    host: `${process.env.API_LOG_GRAFANA_LOKI_URL}`,
-    labels: { app: `${process.env.APP_NAME}`},
-    json: true,
-    format: winston.format.json(),
-    replaceTimestamp: true,
-    onConnectionError: (err) => console.error(err)
-  }))
+if (`${process.env.API_LOG_FILE_LOKI_TRANSPORT_ENABLED}` == '1') {
+  transports.push(logTransports.lokiTransport)
 }
 
-if( `${process.env.API_LOG_CONSOLE_TRANSPORT_ENABLED}` == '1'){
-  transports.push( new winston.transports.Console())
+if (`${process.env.API_LOG_CONSOLE_TRANSPORT_ENABLED}` == '1') {
+  transports.push(logTransports.consoleTransport)
 }
 
 
-if( `${process.env.API_LOG_FILE_TRANSPORT_ENABLED}` == '1'){
-  transports.push(new DailyRotateFile({
-    filename: `${process.env.API_LOG_FILE_AUDIT_NAME}`,
-    dirname: `${process.env.API_LOG_PATH}`,
-    datePattern: 'YYYY-MM-DD',
-    zippedArchive: true,
-    
-    maxSize: `${process.env.API_LOG_MAX_SIZE}`,
-    maxFiles: `${process.env.API_LOG_MAX_DAYS}`
-    }))
+if (`${process.env.API_LOG_FILE_TRANSPORT_ENABLED}` == '1') {
+  transports.push(logTransports.fileTransport)
 }
 
 
-const Logger = winston.createLogger({
+
+let Logger = winston.createLogger({
   level: 'debug',
   levels,
   format,
   transports,
-  exceptionHandlers: [errorTransport]
-})
+  handleExceptions: true,
+  handleRejections: true,
+  exitOnError: false
+});
+
+
+// Set Exception Handler
+if (`${process.env.API_LOG_FILE_LOKI_TRANSPORT_ENABLED}` == '1') {
+  Logger.exceptions.handle(logTransports.lokiTransport);
+} else {
+  Logger.exceptions.handle(errorTransport);
+}
+
 
 export default Logger
